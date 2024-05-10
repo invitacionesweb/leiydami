d347fdc756daf97d092935a54cc9e3d3fc75162c
Ara Garrido <94240265+invitacionesweb@users.noreply.github.com>
2024-05-10 17:19:13 -0300

    AC
    AC

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Establecer la fecha de destino (puedes ajustarla según tus necesidades)
-const fechaDestino = new Date("2024-08-28T23:59:59");
+const fechaDestino = new Date("2024-08-23T23:59:59");
 
 function actualizarContador() {
   const ahora = new Date();
